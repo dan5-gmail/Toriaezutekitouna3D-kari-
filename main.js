235a35f88d88c454d391b90854474bb92b60a095
dan5-gmail <kepa.kachi310@gmail.com>
2026-02-26 01:50:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js'; 
 import { PointerLockControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/PointerLockControls.js';
-
 
 // ===== シーン =====
 const scene = new THREE.Scene();
