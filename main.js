61413ea931093996a09b5d11c7d38333387c2a99
dan5-gmail <kepa.kachi310@gmail.com>
2026-02-26 00:30:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { PointerLookControls } from `https://unpkg.com/three@0.160.0/examples/jsm/controls/PointerLookControls.js`;
+import { PointerLockControls } from `https://unpkg.com/three@0.160.0/examples/jsm/controls/PointerLookControls.js`;
 
 // ===== シーン =====
 const scene = new THREE.Scene();
